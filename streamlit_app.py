42abe1ef8c89eb4da0f5a6b84cddd5a531a06332
AmitDataEnginner <134190024+AmitDataEnginner@users.noreply.github.com>
2023-05-21 00:35:04 -0600

    Update streamlit_app.py
    Added avocado toast in breakfast menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title("My parent's new healthy diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and blueberry Oatmeal.")
-streamlit.text("Kale, Spinach and rocket smoothie.")
-streamlit.text("Hard boiled free-range egg.")
+streamlit.text("🥣Omega 3 and blueberry Oatmeal.")
+streamlit.text("🥗Kale, Spinach and rocket smoothie.")
+streamlit.text("🐔Hard boiled free-range Egg.")
+streamlit.text("🥑🍞Avocado Toast.")
