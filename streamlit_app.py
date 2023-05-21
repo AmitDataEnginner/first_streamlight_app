ef69aba2861a779db5ef41d2c502cc6a5caa5b57
AmitDataEnginner <134190024+AmitDataEnginner@users.noreply.github.com>
2023-05-21 00:33:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My parent's new healthy diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and blueberry Oatmeal.")
+streamlit.text("Kale, Spinach and rocket smoothie.")
+streamlit.text("Hard boiled free-range egg.")
